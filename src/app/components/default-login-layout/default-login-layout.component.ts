aa574812e2202415bd571aff89611c9ab24e50ff
Diego Ramon <diegoramonsm@gmail.com>
2024-07-20 02:22:02 -0300

    FEAT: Adicionando o componente e página de login

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-login-layout.component.html',
+  styleUrls: ['./default-login-layout.component.scss']
+})
+export class DefaultLoginLayoutComponent  {
+
+
+}
+
