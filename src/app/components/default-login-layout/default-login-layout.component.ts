793dce72026ee2d6298918fc95e1aca04ef92997
Diego Ramon <diegoramonsm@gmail.com>
2024-07-20 03:35:24 -0300

    FEAT: Add default layout style

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./default-login-layout.component.scss']
 })
 export class DefaultLoginLayoutComponent  {
-
-
+  @Input() title:string = "";
+  @Input() primaryBtnText:string = "";
+  @Input() secondaryBtnText:string = "";
 }
 
